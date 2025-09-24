0e6f33b8ba7c6827f2d947e354a1c985e4e6e93b
artur <filutekartur@interia.pl>
2025-09-24 21:02:57 +0200

    settings.py updated for local enviroment variables

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()  # take environment variables
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['filutekartur.pythonanywhere.com']
+ALLOWED_HOSTS = ['filutekartur.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
